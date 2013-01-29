0168ea13dcf32dfd0060f4e058a8a2d38196ef18
tnightingale <tom@tnightingale.com>
2013-01-29 12:37:56 -0800

    removed colorbrewer css, cleaned up hex styling example

--- js/quake-hexbin.js
+++ js/quake-hexbin.js
@@ -1,32 +1,35 @@
-var container = L.DomUtil.get('quake'),
-    map = L.map(container).setView([-43.6, 172.3], 10);
+(function () {
+    var max, scale,
+        classes = 9,
+        scheme = colorbrewer["YlOrRd"][classes],
+        container = L.DomUtil.get('quake'),
+        map = L.map(container).setView([-43.6, 172.3], 10);
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-d3.json(container.dataset.source, function(collection) {
-    var hex_layer = L.hexLayer(collection, {
-            applyStyle: hex_style
-        });
+    d3.json(container.dataset.source, function(collection) {
+        var hex_layer = L.hexLayer(collection, {
+                applyStyle: hex_style
+            });
 
-    map.addLayer(hex_layer);
-});
+        map.addLayer(hex_layer);
+    });
 
-function hex_style (hexagons) {
-    var classes = this._container.attr("class");
-    this._container.attr("class", classes + " YlOrRd");
+    function hex_style (hexagons) {
+        // Maintain a density scale relative to initial zoom level.
+        if (!(max && scale)) {
+            max = d3.max(hexagons.data(), function (d) { return d.length; });
+            scale = d3.scale.quantize()
+                    .domain([0, max])
+                    .range(d3.range(classes));
+        }
 
-    var num_classes = 9,
-        max = d3.max(hexagons.data(), function (d) { return d.length; }),
-        scale = d3.scale.quantize()
-            .domain([max, 0])
-            .range(d3.range(num_classes));
-
-    hexagons.attr("stroke", "#800026")
-        .attr("class", function (d) {
-            var num = ((num_classes - 1) - scale(d.length)),
-            c = 'q' + num + "-" + num_classes;
-            return 'hexagon ' + c;
-        });
-}
+        hexagons
+            .attr("stroke", scheme[classes - 1])
+            .attr("fill", function (d) {
+                return scheme[scale(d.length)];
+            });
+    }
+}());
